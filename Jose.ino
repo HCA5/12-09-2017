7ed3cec49edffbda6e8e4ab9f3a5f5cbadfd9631
José Pedro Mello <jose.coelho@usp.br>
2017-09-12 21:11:53 -0300

    Add files via upload

---
+++ Jose.ino
@@ -0,0 +1,21 @@
+int buzpin = 9;
+int ldrpin = 0;
+ 
+void setup()
+{ 
+ Serial.begin(9600);
+  pinMode(9,OUTPUT);
+  
+}
+void loop() 
+{ 
+   int ldr = analogRead(ldrpin); 
+   Serial.println(ldr); 
+   if (ldr < 1000) {
+    Serial.println("BIP"); 
+   tone(9,100); 
+   delay(5);
+   noTone(9); 
+   delay(ldr);
+   }
+}
